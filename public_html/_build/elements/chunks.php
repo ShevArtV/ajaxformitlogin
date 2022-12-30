ea6767a73fb44c62c4e981aab5f308ea0522974c
ShevArtV <shev.art.v@ya.ru>
2022-12-30 20:11:16 +0300

    Add package

--- public_html/_build/elements/chunks.php
+++ public_html/_build/elements/chunks.php
@@ -2,39 +2,39 @@
 
 return [
     'activateEmail' => [
-        'file' => 'emails/activateEmail.tpl',
+        'file' => 'emails/activateEmail',
         'description' => 'пример чанка письма со ссылкой на активацию',
     ],
     'exampleEmail' => [
-        'file' => 'emails/exampleEmail.tpl',
+        'file' => 'emails/exampleEmail',
         'description' => 'пример чанка письма',
     ],
     'exampleForm' => [
-        'file' => 'forms/exampleForm.tpl',
+        'file' => 'forms/exampleForm',
         'description' => 'пример чанка формы',
     ],
-    'forgotForm.tpl' => [
-        'file' => 'forms/forgotForm.tpl',
+    'forgotForm' => [
+        'file' => 'forms/forgotForm',
         'description' => 'пример чанка формы восстановления пароля',
     ],
     'loginForm' => [
-        'file' => 'forms/loginForm.tpl',
+        'file' => 'forms/loginForm',
         'description' => 'пример чанка формы авторизации',
     ],
     'logoutForm' => [
-        'file' => 'forms/logoutForm.tpl',
+        'file' => 'forms/logoutForm',
         'description' => 'пример чанка формы выхода из аккаунта',
     ],
     'registerForm' => [
-        'file' => 'forms/registerForm.tpl',
+        'file' => 'forms/registerForm',
         'description' => 'пример чанка формы регистрации',
     ],
     'updatePassForm' => [
-        'file' => 'forms/updatePassForm.tpl',
+        'file' => 'forms/updatePassForm',
         'description' => 'пример чанка формы изменения пароля',
     ],
     'updateProfileForm' => [
-        'file' => 'forms/updateProfileForm.tpl',
+        'file' => 'forms/updateProfileForm',
         'description' => 'пример чанка формы редактирования личных данных',
     ],
 ];
