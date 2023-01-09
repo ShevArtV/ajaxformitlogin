499cd2ce34e1564e9d34c0b650a1f806190d0648
ShevArtV <shev.art.v@ya.ru>
2023-01-09 11:28:48 +0300

    Add setting on setup

--- public_html/_build/config.inc.php
+++ public_html/_build/config.inc.php
@@ -11,7 +11,7 @@ if (!defined('MODX_CORE_PATH')) {
 return [
     'name' => 'AjaxFormitLogin',
     'name_lower' => 'ajaxformitlogin',
-    'version' => '1.0.1',
+    'version' => '1.0.2',
     'release' => 'pl',
     // Install package to site right after build
     'install' => false,
--- public_html/_build/elements/settings.php
+++ public_html/_build/elements/settings.php
@@ -4,21 +4,21 @@ return [
     'afl_frontend_js' => [
         'xtype' => 'textfield',
         'value' => '[[+assetsUrl]]js/default.js',
-        'area' => 'ajaxformitlogin_main',
+        'area' => 'default',
     ],
     'afl_notify_classname' => [
         'xtype' => 'textfield',
         'value' => 'AflIziToast',
-        'area' => 'ajaxformitlogin_main',
+        'area' => 'default',
     ],
     'afl_notify_classpath' => [
         'xtype' => 'textfield',
         'value' => './aflizitoast.class.js',
-        'area' => 'ajaxformitlogin_main',
+        'area' => 'default',
     ],
     'afl_notify_js' => [
         'xtype' => 'textfield',
         'value' => 'assets/components/ajaxformitlogin/js/message_settings.json',
-        'area' => 'ajaxformitlogin_main',
+        'area' => 'default',
     ],
 ];
